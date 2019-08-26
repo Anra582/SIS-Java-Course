292291ac09d894d9eb4e2d51897bcc5a18947f53
Alexey Black <anra582@gmail.com>
2019-08-26 21:16:17 +0700

    PathFinder test passed

--- unit-5/homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit-5/homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -1,14 +1,19 @@
 package pro.sisit.javacourse;
 
 import pro.sisit.javacourse.entity.DeliveryTask;
+import pro.sisit.javacourse.entity.Route;
 import pro.sisit.javacourse.entity.Transport;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class PathFinder {
 
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
-        return null;
+        return transports.stream().filter(transport -> transport.getVolume() >= deliveryTask.getVolume()
+            && deliveryTask.getRoutes().stream().anyMatch(route -> route.getType() == transport.getType()))
+                .min(Comparator.comparingDouble(Transport::getPrice)).orElseThrow(() -> new RuntimeException("Optimal transport not found."));
+
     }
 
 }
