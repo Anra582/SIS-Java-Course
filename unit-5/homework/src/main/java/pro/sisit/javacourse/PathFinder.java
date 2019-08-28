961949f3cbec951b8de60c5724a44b6cc03bf20e
Alexey Black <anra582@gmail.com>
2019-08-28 22:46:49 +0700

    Rewrite expensive parts of PathFinder::getOptimalTransport into separate methods.

--- unit-5/homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit-5/homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -10,12 +10,20 @@ import java.util.List;
 public class PathFinder {
 
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
-        return transports.stream().filter(transport -> transport.getVolume() >= deliveryTask.getVolume()
-            && deliveryTask.getRoutes().stream().anyMatch(route -> route.getType() == transport.getType()))
-                .min(Comparator.comparingDouble(transport -> transport.getPrice() * deliveryTask.getRoutes()
-                        .stream().filter(route -> route.getType() == transport.getType()).findFirst()
-                        .get().getLength())).orElseThrow(() -> new RuntimeException("Optimal transport not found."));
+        return transports.stream().filter(transport -> isTransportSuitable(transport, deliveryTask))
+                .min(Comparator.comparingDouble(transport -> calcFinalDeliveryCost(transport, deliveryTask)))
+                .orElseThrow(() -> new RuntimeException("Optimal transport not found."));
 
     }
 
+    private static boolean isTransportSuitable(Transport transport, DeliveryTask deliveryTask) {
+        return transport.getVolume() >= deliveryTask.getVolume()
+                && deliveryTask.getRoutes().stream().anyMatch(route -> route.getType() == transport.getType());
+    }
+
+    private static double calcFinalDeliveryCost(Transport transport, DeliveryTask deliveryTask) {
+        return transport.getPrice() * deliveryTask.getRoutes()
+                .stream().filter(route -> route.getType() == transport.getType()).findFirst()
+                .get().getLength();
+    }
 }
